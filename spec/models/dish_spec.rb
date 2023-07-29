a2527ff8e6003a32930e5d88e32219d2ad255cee
Davis Weimer <davisweimer@gmail.com>
2023-07-29 13:58:04 -0600

    User Story 1 complete

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-
+  before(:each) do
+    test_data
+  end
   describe "validations" do
       it {should validate_presence_of :name}
       it {should validate_presence_of :description}
@@ -11,4 +13,11 @@ RSpec.describe Dish, type: :model do
       it {should belong_to :chef}
   end
 
+  describe "instance methods" do
+    it "#total_calorie_count" do
+      expect(@dish_1.total_calorie_count).to eq(140)
+      expect(@dish_2.total_calorie_count).to eq(870)
+      expect(@dish_1.total_calorie_count).to_not eq(1)
+    end
+  end
 end
