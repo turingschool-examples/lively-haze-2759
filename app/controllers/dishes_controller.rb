03bcb84a4c02cd371d87124e882e878473ecec5b
Davis Weimer <davisweimer@gmail.com>
2023-07-29 14:51:52 -0600

    User Story 2 Complete

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,15 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def edit
+    @dish = Dish.find(params[:id])
+  end
+
+  def update
+    dish = Dish.find(params[:id])
+    ingredient = Ingredient.find(params[:dish][:add_ingredient_id])
+    dish.ingredients.push(ingredient)
+    redirect_to dish_path(dish.id)
+  end
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "The Dishes", type: :feature do
     test_data
   end
   describe "#show_page" do
-    it "I see the dish name, description, list of ingredients and total calories" do
+    it "displays the dish name, description, list of ingredients and total calories" do
 
       visit dish_path(@dish_1.id)
-      
+
       expect(page).to have_content("Soupe du jour: Vegetable")
       expect(page).to have_content("Delicious, splendide")
       expect(page).to have_content("Carrot")
@@ -18,6 +18,19 @@ RSpec.describe "The Dishes", type: :feature do
       expect(page).to have_content("Pierre Le Plupe")
 
       expect(page).to_not have_content("Big Mac")
+    end
+
+    it "displays a form to add an existing ingredient to the dish" do
+
+      visit dish_path(@dish_2.id)
+
+      within("#add_ingredient") do
+        fill_in @dish, with: @ingredient_4.id
+        click_button "Update Dish"
+      end
+      expect(page).to have_content("Chicken")
+      expect(@dish_2.ingredients).to eq([@ingredient_1,@ingredient_2,@ingredient_4,@ingredient_6])
+      expect(current_path).to eq(dish_path(@dish_2.id))
     end
   end
 end
