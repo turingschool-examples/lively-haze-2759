530005c27f86dfca07c59bb4b8ac05c80ee0a3fb
Artemy Gibson <artemygibson@gmail.com>
2023-07-29 08:54:58 -0700

    Add update to add ingredients to dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,10 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def update
+    dish = Dish.find(params[:id])
+    DishIngredient.create!(dish: dish, ingredient_id: params[:dish][:add_ingredient])
+    redirect_to dish_path(dish)
+  end
 end
