c5152c8a76deb72a7cf34bd31b0a67c69a347b9c
Derek Chavez <frrenose17@yahoo.com>
2023-07-30 12:44:40 -0600

    test: write test for dish's ingredients list on show page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,7 @@
 class DishesController < ApplicationController
 
    def show
-    @dishes = Dish.find(params[:id])
+    @dish = Dish.find(params[:id])
    end
-   
+
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -15,9 +15,25 @@ RSpec.describe "dishes show page", type: :feature do
       dish_1 = chef.dishes.create!(name: "Rice", description: "Crucial")
 
       visit dish_path(dish_1)
-
+      
       expect(page).to have_content(dish_1.name)
       expect(page).to have_content(dish_1.description)
+    end
+    
+    it "displays a list of ingredients for that dish" do
+      chef = Chef.create!(name: "Derek Chavez")
+      dish_1 = chef.dishes.create!(name: "Rice", description: "Crucial")
+
+      ingredient_1 = Ingredient.create!(name: "Water", calories: 0)
+      ingredient_2 = Ingredient.create!(name: "Salt", calories: 10)
+
+      dish_1.ingredients << ingredient_1
+      dish_1.ingredients << ingredient_2
+
+      visit dish_path(dish_1)
+
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_2.name)
     end
   end
 end
