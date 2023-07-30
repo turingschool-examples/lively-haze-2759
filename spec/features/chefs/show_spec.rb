7d16eeb5e963dc42c880b7fea56ee79241a816fa
Derek Chavez <frrenose17@yahoo.com>
2023-07-30 13:58:35 -0600

    feat: add index method to ingredients controller

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -36,12 +36,40 @@ RSpec.describe "chef's show page", type: :feature do
 
       expect(page).to have_link("Chefs Ingredients")
 
-      # expect(page).to have_content(ingredient_1.name)
-      # expect(page).to have_content(ingredient_2.name)
-      # expect(page).to have_content(ingredient_3.name)
-      # expect(page).to have_content(ingredient_4.name)
-      # expect(page).to have_content(ingredient_5.name)
-      # expect(page).to have_content(ingredient_6.name)
+    end
+
+    it "takes me to the chef's ingredients index page where I see a unique list of ingredients that this chef uses, when the link is clicked" do
+      chef = Chef.create!(name: "Derek Chavez")
+      dish_1 = chef.dishes.create!(name: "Rice", description: "Crucial")
+      dish_2 = chef.dishes.create!(name: "Yummy", description: "Very good")
+
+      ingredient_1 = Ingredient.create!(name: "Water", calories: 0)
+      ingredient_2 = Ingredient.create!(name: "Salt", calories: 10)
+      ingredient_3 = Ingredient.create!(name: "Cilantro", calories: 15)
+      ingredient_4 = Ingredient.create!(name: "Pasta", calories: 100)
+      ingredient_5 = Ingredient.create!(name: "Chicken", calories: 150)
+      ingredient_6 = Ingredient.create!(name: "Pesto", calories: 75)
+
+      dish_1.ingredients << ingredient_1
+      dish_1.ingredients << ingredient_2
+      dish_1.ingredients << ingredient_3
+
+      dish_2.ingredients << ingredient_4
+      dish_2.ingredients << ingredient_1
+      dish_2.ingredients << ingredient_5
+      dish_2.ingredients << ingredient_6
+
+      visit chef_path(chef)
+
+      click_link "Chefs Ingredients"
+
+      expect(page).to have_current_path(chef_ingredients_path(chef))
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_2.name)
+      expect(page).to have_content(ingredient_3.name)
+      expect(page).to have_content(ingredient_4.name)
+      expect(page).to have_content(ingredient_5.name)
+      expect(page).to have_content(ingredient_6.name)
     end
   end
 end
