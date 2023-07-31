cf92621eec9c5cc3ef0d661cdf5fff841d0e1f76
Anna Wiley <awiley33@gmail.com>
2023-07-31 17:53:11 -0500

    test: Add model and feature testing for user story 1

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Dish, type: :model do
       it { should have_many :ingredients }
   end
 
+  describe "instance methods" do
+    describe "#calorie_count" do
+      it "adds up the calories from all ingredients in a dish to display a single integer" do
+        load_test_data
+
+        expect(@dish_1.calorie_count).to eq(540)
+        expect(@dish_2.calorie_count).to eq(350)
+        expect(@dish_3.calorie_count).to eq(490)
+      end
+    end
+  end
+
 end
