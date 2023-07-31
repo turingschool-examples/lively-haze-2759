eca340a1414e2727d516d191dd664692fcf499e8
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-31 16:04:31 -0400

    Refactor: moves add_ingredient method from controller to model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -7,4 +7,18 @@ class Dish < ApplicationRecord
   def calorie_count
     ingredients.sum(:calories)
   end
+
+  def add_ingredient(ingredient_id)
+    if Ingredient.exists?(ingredient_id)
+      ingredient = Ingredient.find(ingredient_id)
+      if ingredients.include?(ingredient)
+        { success: false, message: 'This ingredient is already included in the dish' }
+      else
+        ingredients << ingredient
+        { success: save, message: 'Ingredient added successfully' }
+      end
+    else
+      { success: false, message: 'This ingredient does not exist' }
+    end
+  end
 end
