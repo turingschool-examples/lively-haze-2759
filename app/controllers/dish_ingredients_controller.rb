6217be7233b03ea81c788b63c13f9e69d961a1ae
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-31 16:04:25 -0400

    Refactor: moves add_ingredient method from controller to model

--- app/controllers/dish_ingredients_controller.rb
+++ app/controllers/dish_ingredients_controller.rb
@@ -1,15 +1,8 @@
 class DishIngredientsController < ApplicationController
   def create
     @dish = Dish.find(params[:dish_id])
-    @ingredient = Ingredient.find(params[:ingredient_id])
-
-    if @dish.ingredients.include?(@ingredient)
-      flash[:alert] = 'This ingredient is already included in the dish'
-    else
-      @dish.ingredients << @ingredient
-      flash[:notice] = 'Ingredient added successfully' if @dish.save
-    end
-
+    result = @dish.add_ingredient(params[:ingredient_id])
+    flash[result[:success] ? :notice : :alert] = result[:message]
     redirect_to dish_path(@dish)
   end
 end
