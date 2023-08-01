07fe3871556287faf442c5ffba02a929ed1aa45a
Bradley Milton <bmilton2412@gmail.com>
2023-08-01 12:53:09 -0700

    Adds user story 2 and adds method calorie count to user story 1 I forgot

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -23,9 +23,34 @@ RSpec.describe "dish show page" do
       expect(page).to have_content("Bread")
     end
 
+    within("#calorie-count") do
+      expect(page).to have_content("Calories")
+      expect(dish.calorie_count).to eq(150)
+    end
+
     within("#chef") do
       expect(page).to have_content("Chef")
       expect(page).to have_content("Name: Gordon")
     end
+  end
+
+  it "has a form to add an existing ingredient to the dish" do
+    chef = Chef.create(name: "Gordon")
+    dish = Dish.create(name: "Grilled Cheese", description: "A tasty dish", chef: chef)
+    ingredient1 = Ingredient.create(name: "Cheese", calories: 50)
+    ingredient2 = Ingredient.create(name: "Bread", calories: 100)
+
+    visit dish_path(dish)
+
+    within("#add-ingredient") do
+      expect(page).to have_content("Add an Ingredient")
+      expect(page).to have_field("dish[ingredient_id]") # Ensure the field with name="dish[ingredient_id]" is present
+      fill_in("dish[ingredient_id]", with: ingredient1.id) # Use the ID of an existing ingredient
+      click_on "Add Ingredient"
+    end
+
+
+    expect(current_path).to eq(dish_path(dish))
+    expect(page).to have_content("Cheese")
   end
 end
