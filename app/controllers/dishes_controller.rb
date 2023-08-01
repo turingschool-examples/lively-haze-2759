a7fd3efb0c3195f8f018b13e082413d75c4e5e11
Connor Richmond <richmond.connor@gmail.com>
2023-08-01 18:34:18 -0400

    update method and route

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,5 +3,11 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def update
+    dish = Dish.find(params[:id])
+    DishIngredient.create!(dish: dish, ingredient_id: params[:dish][:add_ingredient])
+    redirect_to dish_path(dish)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :dishes, only: [:show]
+  resources :dishes, only: [:show, :update]
   
 end
 
