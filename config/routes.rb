c9fdcf74f931dadf2a08b6d566bd1d694e931a75
Jorja Fleming <aset284@gmail.com>
2023-08-01 16:11:03 -0600

    Feat: completed story 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  resources :ingredients, only: [:index]
   resources :dish, only: [:show, :update] 
+  resources :chef, only: [:show] do
+    member do
+      get 'ingredients', to: 'ingredient#index' 
+      # hey I originally used an ingredient controller method in a chef controller route, 
+      # but using ingredient#index seemed more railsy but i could be wrong
+    end
+  end
 end
