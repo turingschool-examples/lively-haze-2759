e2ddf3ca46fce8346e6fd88aab0cd15a4f3ed6c5
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-01 11:35:59 -0800

    dish show page features

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "The Dish Show Page" do
+  before(:each) do
+    @chef = Chef.create!(name: "King Charles")
+    @dish = Dish.create!(name: "Pizza", description: "the good good", chef: @chef)
+    @dough = Ingredient.create!(name: "Dough", calories: 420)
+    @sauce = Ingredient.create!(name: "Sauce", calories: 142)
+    @cheese = Ingredient.create!(name: "Cheese", calories: 69)
+    DishIngredient.create!(dish: @dish, ingredient: @dough)
+    DishIngredient.create!(dish: @dish, ingredient: @sauce)
+    DishIngredient.create!(dish: @dish, ingredient: @cheese)
+  end
+
+  it 'shows the dish name and description' do
+    puts @dish.inspect
+    visit dishes_path(@dish)
+    save_and_open_page
+    expect(page).to have_content("Pizza")
+    expect(page).to have_content("the good good")
+  end
+
+  it 'shows the dish ingredients' do
+    visit dishes_path(@dish.id)
+    expect(page).to have_content("Dough")
+    expect(page).to have_content("Sauce")
+    expect(page).to have_content("Cheese")
+  end
+
+  it 'shows the total calories of the dish' do
+    visit dishes_path(@dish.id)
+    expect(page).to have_content("Total: 631 calories")
+  end
+
+  it 'shows the chefs name' do
+    visit dishes_path(@dish.id)
+    expect(page).to have_content("Chef: King Charles")
+  end
+end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Dish, type: :model do
       it {should have_many :dish_ingredients}
   end
 
+  describe "#total_calories" do
+    before do
+      @chef = Chef.create!(name: "King Charles")
+      @dish = Dish.create!(name: "Pizza", description: "the good good", chef: @chef)
+      @dough = Ingredient.create!(name: "Dough", calories: 420)
+      @sauce = Ingredient.create!(name: "Sauce", calories: 142)
+      @cheese = Ingredient.create!(name: "Cheese", calories: 69)
+      DishIngredient.create!(dish: @dish, ingredient: @dough)
+      DishIngredient.create!(dish: @dish, ingredient: @sauce)
+      DishIngredient.create!(dish: @dish, ingredient: @cheese)
+    end
+
+    it "returns dish's total calories" do
+      expect(@dish.total_calories).to eq(420 + 142 + 69)
+    end
+  end
+
 end
