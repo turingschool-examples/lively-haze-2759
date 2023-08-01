f59ad1799232a928279ed8c36141dd7de3762157
Kaina Cockett <kainacockett@gmail.com>
2023-08-01 06:38:44 -0600

    feat: US1 complete

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -6,30 +6,34 @@ describe "Dish Show page" do
     @dish_1 = Dish.create!(name: "Chicken Tenders Plate", description: "Smaller portion of the Chicken Tenders Platter. Crispy breaded chicken tenders are a grill and bar classic. Served with choice of dipping sauce. Shown with classic fries.", chef_id: "#{@chef_1.id}")
     @fries_1 = Ingredient.create!(name: "Fries", calories: 400)
     @tenders_sm = Ingredient.create!(name: "Chicken Tenders small", calories: 1080)
+    @dish_1.ingredients << @fries_1
+    @dish_1.ingredients << @tenders_sm
   end
   describe "As a visitor When I visit a dish's show page" do
     it "I see the dish’s name and description" do
-      visit dish_show_path
-      
-      expect(page).to have_content("Chicken Tenders Plate")
-      expect(page).to have_content("Smaller portion of the Chicken Tenders Platter. Crispy breaded chicken tenders are a grill and bar classic. Served with choice of dipping sauce. Shown with classic fries.")
+      visit dish_path("#{@dish_1.id}")
+      save_and_open_page
+      expect(page).to have_content("#{@dish_1.name}")
+      expect(page).to have_content("#{@dish_1.description}")
       
     end
     it "And I see a list of ingredients for that dish" do
-      visit dish_show_path
+      visit dish_path("#{@dish_1.id}")
       
-      expect(page).to have_content("#{@tenders_sm}")
-      expect(page).to have_content("#{@fries_1}")
+      expect(page).to have_content("#{@tenders_sm.name}")
+      expect(page).to have_content("#{@fries_1.name}")
     end
     it "and a total calorie count for that dish" do
-      visit dish_show_path
+      visit dish_path("#{@dish_1.id}")
       
       expect(page).to have_content("1480")
     end
     it "And I see the chef's name" do
-      visit dish_show_path
+      visit dish_path("#{@dish_1.id}")
       
-      expect(page).to have_content("#{@chef_1}")
+      expect(page).to have_content("#{@chef_1.name}")
     end
+    
+    
   end
 end
