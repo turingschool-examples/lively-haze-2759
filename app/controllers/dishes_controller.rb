3b40e5e36a03ec809cdd6928f78996dffd78e0ed
Matthew Lim <matlim200@gmail.com>
2023-07-31 21:42:24 -0700

    Feat: Add all tests and functions for user story 2

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,12 @@ class DishesController < ApplicationController
   def index
     @dish = Dish.find(params[:id])
   end
+
+  def update
+    @dish = Dish.find(params[:id])
+    @ingredient = Ingredient.find(params[:ingredient_id])
+    @dish.ingredients << @ingredient
+    @dish.save
+    redirect_to "/dishes/#{@dish.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/dishes/:id", to: "dishes#index"
+  patch "/dishes/:id", to: "dishes#update"
 end
