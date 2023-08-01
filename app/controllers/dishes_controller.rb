c63bb5233a3e21ef0753cf0a1e57f4beb36b98b3
Allen Russell <garyrusselljr@gmail.com>
2023-08-01 13:18:24 -0600

    fix: issue with tabs when using git restore

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,6 @@
 class DishesController < ApplicationController
- def show
-  @dish = Dish.find(params[:id])
-  @total_calories = @dish.total_calories
- end
+  def show
+    @dish = Dish.find(params[:id])
+    @total_calories = @dish.total_calories
+  end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,22 +1,22 @@
 class Chef < ApplicationRecord
-   validates_presence_of :name
-   
-   has_many :dishes
-   has_many :ingredients, through: :dishes
+  validates_presence_of :name
+  
+  has_many :dishes
+  has_many :ingredients, through: :dishes
 
 
-   def ingredient_names
-      ingredients.select(:name)
-      .distinct
-      .pluck(:name)
+  def ingredient_names
+     ingredients.select(:name)
+     .distinct
+     .pluck(:name)
+  end
+
+  def top_3_ingredients
+     Ingredient.joins(:dishes)
+        .where(dishes: { chef_id: id })
+        .group(:id)
+        .order('COUNT(dishes.id) DESC')
+        .limit(3)
+        .pluck(:name)
    end
-
-   def top_3_ingredients
-      Ingredient.joins(:dishes)
-         .where(dishes: { chef_id: id })
-         .group(:id)
-         .order('COUNT(dishes.id) DESC')
-         .limit(3)
-         .pluck(:name)
-    end
 end
