63a8de8f47206b0ab8908a16f1693b8a0420a248
Ian Lyell <ian.lyell@yahoo.com>
2023-08-01 00:59:42 -0500

    Finish relationship and db setup

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Ingredient, type: :model do
 
     describe 'relationships' do
-        it {should have_many(:dish_ingedients)}
-        it {should have_many(:dishes).through(:dish_ingedients)}
+        it {should have_many(:dish_ingredients)}
+        it {should have_many(:dishes).through(:dish_ingredients)}
     end
 end
