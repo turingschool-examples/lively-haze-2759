6e6465662f5a9c794a13fca4be2819fa50f00337
Bradley Milton <bmilton2412@gmail.com>
2023-08-01 11:46:52 -0700

    adds user story 1 with fixed associations to ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,4 @@
 class Ingredient < ApplicationRecord
-  validates_presence_of :name, :description
-  belongs_to :chef
   has_many :dish_ingredients
   has_many :dishes, through: :dish_ingredients
 end
